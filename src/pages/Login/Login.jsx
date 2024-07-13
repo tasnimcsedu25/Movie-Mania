041586d3f06a50461005432340ad81192042da86
tasnimcsedu25 <tasnimcsedu25@gmail.com>
2024-07-13 23:54:55 +0600

    sign in & sign up page layout done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
 const Login = () => {
+  const [signState,setSignState]=useState("Sign In")
+
   return (
     <div className='login'>
-      
+      <img src={logo} className='login-logo'/>
+      <div className="login-form">
+        <h1>{signState}</h1>
+        <form>
+          {signState==="Sign Up"?<input type='text' placeholder='Your name'/>:<></>}
+          
+          <input type='email' placeholder='Email '/>
+          <input type='password' placeholder='Password'/>
+          <button>{signState}</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type='checkbox'/>
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need help?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          {signState==="Sign In"?
+          <p>New to Movie Mania? <span onClick={()=>{setSignState("Sign Up")}}>Sign Up Now</span></p>
+          :<p>Already have account?<span onClick={()=>{setSignState("Sign In")}}>Sign In now</span></p>
+        }
+          
+          
+        </div>
+      </div>
     </div>
   )
 }
