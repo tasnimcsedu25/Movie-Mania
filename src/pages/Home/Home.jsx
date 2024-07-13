9dd6350fea55cee49bb77d191e41f45f65ef1396
tasnimcsedu25 <tasnimcsedu25@gmail.com>
2024-07-13 14:00:21 +0600

    navbar complete

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../Compnents/Navbar/Navbar'
+const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
