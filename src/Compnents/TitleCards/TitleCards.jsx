7ebd376e7789c5cd8e1d8018247ef509d322e051
tasnimcsedu25 <tasnimcsedu25@gmail.com>
2024-07-13 21:34:01 +0600

    titles cards done

--- src/Compnents/TitleCards/TitleCards.jsx
+++ src/Compnents/TitleCards/TitleCards.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{useEffect, useRef} from 'react'
 import './TitleCards.css'
-const TitleCards = () => {
+import cards_data from '../../assets/cards/Cards_data'
+
+
+const TitleCards = ({title,category}) => {
+  const cardsRef=useRef();
+const handleWheel=(event)=>{
+event.preventDefault();
+cardsRef.current.scrollLeft+=event.deltaY;
+}
+useEffect(()=>{
+  cardsRef.current.addEventListener('wheel',handleWheel)
+},[])
   return (
-    <div>
-      
+    <div className='title-cards'>
+      <h2>{title?title:"Popular on Movie Mania"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card,index)=>{
+            return <div className="card" key={index}>
+              <img src={card.image}/>
+              <p>{card.name}</p>
+            </div>
+        })}
+      </div>
     </div>
   )
 }
